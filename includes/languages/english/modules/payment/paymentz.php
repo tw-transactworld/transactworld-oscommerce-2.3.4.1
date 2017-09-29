94a4807167b65e056a9a9d9f7494129552bbb673
tw-transactworld <haresh.panchal@paymentz.com>
2017-09-29 12:50:07 +0530

    TransactWorld oscommerce updated files commited

---
+++ includes/languages/english/modules/payment/paymentz.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Developed by TransactWorld.com
+ * Version: 1.0.0
+ * Do not change any code with permission of TransactWorld.com
+ */
+
+
+
+define('MODULE_PAYMENT_PAYMENTZ_TEXT_TITLE', '<div style=" display: inline-flex;">TransactWorld '."[".MODULE_PAYMENT_TRANSACTWORLD_DESCRIPTION."]".'</span></div>');
+
+
+define('MODULE_PAYMENT_PAYMENTZ_TEXT_DESCRIPTION', ' Please make your check or money order payable to:<br />' . MODULE_PAYMENT_TRANSACTWORLD_MERCHANT_ID . '<br />');
+define('MODULE_PAYMENT_PAYMENTZ_TITLE_TEXT', 'TransactWorld');
+
+define('MODULE_PAYMENT_PAYMENTZ_TEXT_ERROR', 'Credit Card Error!');
+
+define('MODULE_PAYMENT_PAYMENTZ_TEXT_ERROR_MESSAGE', 'There has been an error processing you credit card at TransactWorld, please try again.');
+
+define('MODULE_PAYMENT_PAYMENTZ_ALERT_ERROR_MESSAGE', 'Security Error. Illegal access detected, please try again.');
+
+define('MODULE_PAYMENT_PAYMENTZ_CANCEL_ERROR_MESSAGE', 'Thank you for shopping with us.We will keep you posted regarding the status of your order through e-mail');
+
+define('MODULE_PAYMENT_PAYMENTZ_FAILED_ERROR_MESSAGE', 'Thank you for shopping with us. However your transaction failed');
+
+define('MODULE_PAYMENT_PAYMENTZ_TEXT_EMAIL_FOOTER', "Please make your check or money order payable to:" . "\n\n" . MODULE_PAYMENT_TRANSACTWORLD_MERCHANT_ID . "\n");\define('MODULE_PAYMENT_TRANSACTWORLD_DESCRIPTION', ' Enable TransactWorld Online Payments');
+
+?>
+
